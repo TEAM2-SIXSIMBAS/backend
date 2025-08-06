35305d788ba8f037e85c637c553dad3aa6491253
장주은 <jueun3756@gmail.com>
2025-08-06 13:37:07 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/org/example/schoolallianceinfor/SchoolAllianceInforApplication.java
@@ -0,0 +1,13 @@
+package org.example.schoolallianceinfor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAllianceInforApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolAllianceInforApplication.class, args);
+    }
+
+}
